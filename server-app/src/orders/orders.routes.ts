88a7e83f8fb3f8a75c5da6314e4a94f1b0307145
Max Shymchuk <octorix@mail.ru>
2020-02-19 01:47:22 +0300

    feat: remove template from price

--- server-app/src/orders/orders.routes.ts
+++ server-app/src/orders/orders.routes.ts
@@ -1,10 +1,7 @@
 import * as orderController from './orders.service';
-import * as qs from 'qs';
 
 import { Router, Request, Response } from 'express';
-import { UserOrderInput } from './orders.models';
 import { config } from '../config';
-import { getVehicleType } from '../vehicles/vehicles.service';
 
 export const router = Router();
 
@@ -40,16 +37,8 @@ router.get('/price', (req: Request, res: Response) => {
 });
 
 router.get('/price/:order_params', async (req: Request, res: Response) => {
-  const orderParams: UserOrderInput = {
-    from: 'Gomel',
-    to: 'Minsk',
-    who: 'octorix',
-    vehicle: getVehicleType('Car'),
-    cargos: ['Tablet', 'Mango'],
-    message: 'Be careful!'
-  };
-  const params = qs.stringify(orderParams);
-  const result = await orderController.getOrderPrice(params);
+  console.log(req.params.order_params);
+  const result = await orderController.getOrderPrice(req.params.order_params);
   res
     .header('Access-Control-Allow-Origin', '*')
     .status(200)
