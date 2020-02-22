3450a27157ce317dbbbab94596b1ce1bc44ec022
Max Shymchuk <octorix@mail.ru>
2020-02-22 13:54:06 +0300

    feat: add plus/minus button to control time flow

---
+++ client-app/src/components/TimeChanger/TimeChanger.tsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { Button, TextField } from '@material-ui/core';
+import './timeChanger.scss';
+import { changeTime } from '../../services/timeChanger.service';
+import cogoToast from 'cogo-toast';
+
+class TimeChanger extends Component {
+  state = {
+    value: ''
+  };
+
+  componentDidMount() {
+    this.setState(state => ({
+      value: 1
+    }));
+  }
+
+  handleChange = (route: string) => {
+    changeTime(`/${route}/${this.state.value}`);
+    cogoToast.success(`${route} ${this.state.value} day(s)!`, {
+      hideAfter: 1,
+      position: 'bottom-right'
+    });
+  };
+
+  handleInput = (event: any) => {
+    const target = event.target;
+    if (target.value > 0)
+      this.setState(state => ({
+        value: target.value
+      }));
+  };
+
+  render() {
+    return (
+      <section className='changer'>
+        <Button color='inherit' onClick={() => this.handleChange('Minus')}>
+          Minus
+        </Button>
+        <TextField
+          size='small'
+          className='changer-days'
+          type='number'
+          value={this.state.value}
+          onChange={this.handleInput}
+          variant='outlined'
+        />
+        <Button color='inherit' onClick={() => this.handleChange('Plus')}>
+          Plus
+        </Button>
+      </section>
+    );
+  }
+}
+
+export default TimeChanger;
