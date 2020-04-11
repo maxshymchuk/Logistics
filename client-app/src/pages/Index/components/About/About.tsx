6349cb4814476129cab933f80958850a9c4dfa69
Max Shymchuk <octorix@mail.ru>
2020-04-11 03:50:13 +0300

    feat: redesign process section & update css vars

--- client-app/src/pages/Index/components/About/About.tsx
+++ client-app/src/pages/Index/components/About/About.tsx
@@ -16,7 +16,7 @@ const About = () => (
       <Typography variant="h5" component="article">
         Robots, robots and more robots!
       </Typography>
-      <Typography component="p" className={styles.paragraph}>
+      <Typography component="p">
         Created in agony with the dream of the impossible in symbiosis with many scientific organizations,
         a company specializing in convenient, fast and reliable delivery from one point of the world to another.
       </Typography>
@@ -26,12 +26,12 @@ const About = () => (
       <Typography variant="h5" component="article">
         Features
       </Typography>
-      <Typography component="p" className={styles.paragraph}>
+      <Typography component="p">
         The advantage of our company is that almost everything is automated and robotic.
-        In collaboration with <em>General Atomics International</em> and <em>RobCo Industries</em>, robots have been developed
-        for loading or unloading Mr. Handy and Securitron. The Securitron model has been retrofitted for warehouse
-        operations. Equipped with a quantum AI chip, they are able to adequately communicate with people and
-        understand their desires.
+        In collaboration with <em>General Atomics International</em> and <em>RobCo Industries</em>,
+        robots have been developed for loading or unloading &quot;Mr. Handy&quot; and &quot;Securitron&quot;.
+        The &quot;Securitron&quot; model has been retrofitted for warehouse operations. Equipped with a quantum AI
+        chip, they are able to adequately communicate with people and understand their desires.
       </Typography>
       <div className={styles.eye} />
     </section>
@@ -39,9 +39,9 @@ const About = () => (
       <Typography variant="h5" component="article">
         People
       </Typography>
-      <Typography component="p" className={styles.paragraph}>
+      <Typography component="p">
         Although we have the most robotic company, people also found places here. About 10% of our staff are people.
-        For more effective work of human personnel, they are provided with a special outer shell "T3000", which is
+        For more effective work of human personnel, they are provided with a special outer shell &quot;T3000&quot;, which is
         an exoskeleton that increases the strength, endurance and security of the carrier.
       </Typography>
       <div className={styles.armor} />
